0039beac34b6f896d1b87e438b510ec5dc597ba8
PSJ <51746608+psjbk@users.noreply.github.com>
2020-12-07 18:52:40 +0530

    Added Day 3 Part 1.

--- codes/Day 3/AoCD3P1.java
+++ codes/Day 3/AoCD3P1.java
@@ -12,37 +12,61 @@ public class AoCD3 {
         {System.out.println("File not found.");
             fis=null;
         }
+        AoCD3 doTask=new AoCD3();
+        String input= doTask.ftoString(fis),obj;
+        _2DMap twodmap=new _2DMap(input);
+        int cx=3,cy=1,trees=0,t;
+        for(int i=0;i<input.length();i++,cx+=3,cy++)
+        {
+            t=cx%(twodmap.getChildrenPerLine()-1);
+            try {
+                obj = twodmap.getObjectByRowCol(cy, t);
+                if(obj.equals("#"))
+                    trees++;
+            }
+            catch (Exception e)
+            {break;}
+        }
+
+        System.out.println("Trees Encountered : "+trees);
+    }
+
+    private String ftoString(FileInputStream fileInputStream)
+    {
+        String inp="";
+        int i=0;
+        try {
+            while (i!=-1)
+            {
+                i=fileInputStream.read();
+                inp=inp.concat(Character.toString((char)i));
+            }
+            fileInputStream.close();
+        }
+        catch (Exception e)
+        {System.out.println("File not read.");}
+        return inp;
     }
 }
 class _2DMap
 {
-    private String map;
+    private final String map;
     //Please Note a line separator(i.e \n) is important to use this class or a SeparatorNotFoundException is thrown.
     _2DMap(String map)
     { this.map=map; }
 
     public String getObjectByRowCol(int rowNo,int colNo) throws SeparatorNotFoundException
     {
-        String obj="";
+        String obj;
         if (!map.contains("\n"))
             throw new SeparatorNotFoundException("Expected a line separator.");
-        String shape=getShape();
-        int maxCordPerLine=getChildrenPerLine();
-        int lineNum=0;//TODO from here.
-
-        return null;
+        if (rowNo>map.split("\n").length||colNo>getChildrenPerLine())
+            throw new SeparatorNotFoundException("Co-ordinates are invalid.");
+        obj=""+map.split("\n")[rowNo].charAt(colNo);
+        return obj;
     }
 
-    private String getShape()
-    {
-        String[] lineMap=map.split("\n");
-        if (lineMap.length==lineMap[0].length())
-            return "Square";
-        else
-            return "Rectangle";
-    }
-
-    private int getChildrenPerLine() {return map.split("\n")[0].length();}
+    public int getChildrenPerLine() {return map.split("\n")[0].length();}
 }
 
 class SeparatorNotFoundException extends Exception
